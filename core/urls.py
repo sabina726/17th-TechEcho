d078903539d94ff83d2f74b8d230500482da3a1f
chen weichen <newsforcwc@gmail.com>
2024-08-28 15:19:56 +0800

    feat: setup database, change settings and README title

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
