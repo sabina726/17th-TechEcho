54c98dcbb4d1ab0e4c22c0ed8f2d3ce4ee3d5a5c
buding <buding033171@gmail.com>
2024-08-30 16:29:27 +0800

    perf: Add 'introduce' content with if-else logic

--- teacher/forms/teacher_form.py
+++ teacher/forms/teacher_form.py
@@ -8,5 +8,12 @@ class TeacherInfoForm(forms.ModelForm):
         model = TeacherInfo
         fields = ["user", "expertise", "introduce"]
         widgets = {
-            "introduce": forms.Textarea(attrs={"rows": 5}),
+            "introduce": forms.Textarea(
+                attrs={
+                    "rows": 5,
+                    "cols": 40,
+                    "placeholder": "自我介紹",
+                }
+            ),
+            "expertise": forms.TextInput(attrs={"placeholder": "填寫專業能力"}),
         }
