eef93027324e22ccb3b62857416703e294e01e9a
chi <a27976566@gmail.com>
2024-08-30 18:28:14 +0800

    feat: new tag

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,26 @@
-import "./fontawesome"
+import "./fontawesome";
 import Alpine from 'alpinejs';
+import Tagify from '@yaireo/tagify';
+import '@yaireo/tagify/dist/tagify.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("DOM fully loaded and parsed");
+    var input = document.querySelector('#tags');
+    if (input) {
+        console.log("Input element found", input);
+        new Tagify(input, {
+            whitelist: ["JavaScript", "Python", "Ruby", "Java", "C++"],
+            maxTags: 10,
+            dropdown: {
+                maxItems: 20,
+                classname: "tags-look",
+                enabled: 0,
+                closeOnSelect: false
+            }
+        });
+    } else {
+        console.log("Input element not found");
+    }
+});
 
 Alpine.start();
