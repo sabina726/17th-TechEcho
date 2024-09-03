e238d56340df32a97f165cbb1bf3092794ad3196
tony <tony.t0705@gmail.com>
2024-09-03 11:37:50 +0000

    fix: corrected navbar login path and restore home-page access

--- home/migrations/0002_delete_question.py
+++ home/migrations/0002_delete_question.py
@@ -5,12 +5,10 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("home", "0001_initial"),
-
     ]
 
     operations = [
         migrations.DeleteModel(
             name="Question",
-
         ),
     ]
