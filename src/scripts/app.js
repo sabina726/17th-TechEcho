b0bfb8242e76c27c532e05e6b0718f1aa304ce4b
chi <a27976566@gmail.com>
2024-09-04 20:29:52 +0800

    fix: delete unexecutable code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,6 @@
 import "./fontawesome";
 import Alpine from 'alpinejs';
-import Tagify from '@yaireo/tagify';
 import Swal from "sweetalert2";
-
-var input = document.querySelector('input[name=tags-outside]')
-
-var tagify = new Tagify(input, {
-    whitelist: ['Python', 'JavaScript', 'Ruby'],
-    dropdown: {
-        position: 'input',
-        enabled: 0
-    }
-})
 
 window.Swal = Swal;
 Alpine.start();
