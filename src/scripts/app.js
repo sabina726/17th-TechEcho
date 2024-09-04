aab3c25f033a358409ace0aa9c1e8a2de757a22d
chi <a27976566@gmail.com>
2024-09-04 20:11:44 +0800

    build: line-pay add in to poetry list

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,9 +2,16 @@ import "./fontawesome";
 import Alpine from 'alpinejs';
 import Tagify from '@yaireo/tagify';
 import Swal from "sweetalert2";
-import '@yaireo/tagify/dist/tagify.css';
 
-// 暫時拔掉tagify
+var input = document.querySelector('input[name=tags-outside]')
+
+var tagify = new Tagify(input, {
+    whitelist: ['Python', 'JavaScript', 'Ruby'],
+    dropdown: {
+        position: 'input',
+        enabled: 0
+    }
+})
 
 window.Swal = Swal;
 Alpine.start();
