5597b34ffc87c5a51c92bc17f2b6ccd2fe8ebe22
hong <sabina880731@gmail.com>
2024-09-06 14:54:52 +0800

    style: delete commit

--- home/views.py
+++ home/views.py
@@ -3,9 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     if request.user.is_authenticated:
-        # 用戶已登入，傳遞用戶信息到模板
         context = {"user": request.user, "is_authenticated": True}
     else:
-        # 用戶未登入，傳遞相應的上下文
         context = {"is_authenticated": False}
     return render(request, "home/index.html", context)
