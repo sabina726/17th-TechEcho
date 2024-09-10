7305fed7cb8b39fdd8a550aa053a73b932c79f54
buding <buding033171@gmail.com>
2024-09-10 17:28:49 +0800

    fix: Modify the ID floating problem of teacher chat room

--- teachers/models.py
+++ teachers/models.py
@@ -3,7 +3,7 @@ import re
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxLengthValidator, MinLengthValidator
-from django.db import models
+from django.db import IntegrityError, models
 from django.utils import timezone
 
 from answers.models import Answer
@@ -15,16 +15,6 @@ def verify(value):
     # 正則表達式：允許中文字符、英文字符和空格
     if not re.match(r"^[\u4e00-\u9fffA-Za-z\s]*$", value):
         raise ValidationError("專業能力只能包含中英文字符")
-
-
-def generate_unique_group_name(base_name):
-    """Generates a unique group name by appending a number if necessary."""
-    group_name = base_name
-    counter = 1
-    while ChatGroup.objects.filter(group_name=group_name).exists():
-        group_name = f"{base_name}-{counter}"
-        counter += 1
-    return group_name
 
 
 class Teacher(models.Model):
@@ -46,21 +36,23 @@ class Teacher(models.Model):
     )
 
     def save(self, *args, **kwargs):
-        # 如果是新紀錄，則設定 user 為 teacher 並保存
         if not self.pk:
             super().save(*args, **kwargs)
             self.user.is_teacher = True
             self.user.save()
 
-        # 如果沒有 chat_group，則創建一個新的群組
-        if not self.chat_group:
-            base_name = self.nickname or self.user.username
-            unique_group_name = generate_unique_group_name(base_name)
-            chat_group = ChatGroup.objects.create(group_name=unique_group_name)
-            self.chat_group = chat_group
-            self.chat_group.save()
-        # 不再更新已存在的 chat_group 名稱
+        if self.user.is_teacher and not self.chat_group:
+            group_name = f"{self.user.username}_group_{self.id or ''}"
+            try:
+                self.chat_group = ChatGroup.objects.create(group_name=group_name)
+            except IntegrityError:
+                self.chat_group = ChatGroup.objects.get(group_name=group_name)
+
         super().save(*args, **kwargs)
+
+        if self.chat_group and not self.chat_group_id:
+            self.chat_group_id = self.chat_group.id
+            super().save(update_fields=["chat_group_id"])
 
     def __str__(self):
         return f"{self.user.username} - {self.expertise}"
