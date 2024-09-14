9864531370d9fd10bc83dabc4ac3786977e291e8
alex <alexyongxinlin@gmail.com>
2024-09-14 14:50:00 +0800

    feat: add save draft

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,21 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class BlogQuerySet(models.QuerySet):
+    def drafts(self):
+        return self.filter(is_draft=True)
+
+    def published(self):
+        return self.filter(is_draft=False)
 
 
 class Blog(models.Model):
@@ -7,6 +23,17 @@ class Blog(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blogs"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    views = models.IntegerField(default=0)
+    comments_count = models.IntegerField(default=0)
+    is_draft = models.BooleanField(default=True)
+
+    objects = BlogQuerySet.as_manager()
 
     def __str__(self):
         return self.title
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BlogForm
@@ -5,26 +6,56 @@ from .models import Blog
 
 
 def index(request):
-    blogs = Blog.objects.all().order_by("-created_at")
+    blogs = Blog.objects.filter(is_draft=False)  # Get only published blogs
     return render(request, "blogs/index.html", {"blogs": blogs})
 
 
-def show(request, pk):
-    blog = get_object_or_404(Blog, pk=pk)
-    return render(request, "blogs/show.html", {"blog": blog})
+@login_required
+def user_drafts(request):
+    # Get only drafts created by the current user
+    drafts = Blog.objects.filter(author=request.user, is_draft=True)
+    return render(request, "blogs/user_drafts.html", {"drafts": drafts})
 
 
+@login_required
 def new(request):
     if request.method == "POST":
         form = BlogForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("blogs:index")
+            blog = form.save(commit=False)  # Do not save to the database yet
+            blog.author = (
+                request.user
+            )  # Assign the current logged-in user as the author
+
+            action = request.POST.get("action")
+
+            if action == "save_draft":
+                blog.is_draft = True  # Save as a draft
+                blog.save()
+                return redirect("blogs:index")
+
+            elif action == "preview":
+                # Just render the preview page without saving
+                return render(request, "blogs/preview.html", {"blog": blog})
+
+            elif action == "publish":
+                blog.is_draft = False  # Publish the blog
+                blog.save()
+                return redirect("blogs:index")
+
     else:
         form = BlogForm()
     return render(request, "blogs/new.html", {"form": form})
 
 
+def show(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    blog.views += 1
+    blog.save()
+    return render(request, "blogs/show.html", {"blog": blog})
+
+
+@login_required
 def edit(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
     if request.method == "POST":
@@ -37,6 +68,7 @@ def edit(request, pk):
     return render(request, "blogs/edit.html", {"form": form, "blog": blog})
 
 
+@login_required
 def delete(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
     if request.method == "POST":
