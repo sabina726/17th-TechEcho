588bd0bd60c1c228ed6606e1ca40e09c6db03eb9
alex <alexyongxinlin@gmail.com>
2024-09-14 13:04:50 +0800

    fix: fix the blog edit url redirect

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,7 +6,7 @@ app_name = "blogs"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:pk>", views.show, name="show"),
+    path("<int:pk>/", views.show, name="show"),
     path("new/", views.new, name="new"),
     path("<int:pk>/edit/", views.edit, name="edit"),
     path("<int:pk>/delete/", views.delete, name="delete"),
--- blogs/views.py
+++ blogs/views.py
@@ -5,13 +5,11 @@ from .models import Blog
 
 
 def index(request):
-    # Fetch all blog entries ordered by creation date
     blogs = Blog.objects.all().order_by("-created_at")
     return render(request, "blogs/index.html", {"blogs": blogs})
 
 
 def show(request, pk):
-    # Get a specific blog by its primary key (pk)
     blog = get_object_or_404(Blog, pk=pk)
     return render(request, "blogs/show.html", {"blog": blog})
 
@@ -28,7 +26,6 @@ def new(request):
 
 
 def edit(request, pk):
-    # Get a specific blog to edit by its primary key (pk)
     blog = get_object_or_404(Blog, pk=pk)
     if request.method == "POST":
         form = BlogForm(request.POST, instance=blog)
@@ -37,11 +34,10 @@ def edit(request, pk):
             return redirect("blogs:show", pk=blog.pk)
     else:
         form = BlogForm(instance=blog)
-    return render(request, "blogs/edit.html", {"form": form})
+    return render(request, "blogs/edit.html", {"form": form, "blog": blog})
 
 
 def delete(request, pk):
-    # Get a specific blog to delete by its primary key (pk)
     blog = get_object_or_404(Blog, pk=pk)
     if request.method == "POST":
         blog.delete()
