a9b0fa2113b08a1a831dfc53ae3f19e839fa81f2
chen weichen <newsforcwc@gmail.com>
2024-09-15 19:47:06 +0800

    feat: show who is the teacher of the chatroom

--- chat/migrations/0004_remove_chatgroup_members_online_and_more.py
+++ chat/migrations/0004_remove_chatgroup_members_online_and_more.py
@@ -7,18 +7,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('chat', '0003_chatgroup_members_online'),
+        ("chat", "0003_chatgroup_members_online"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='chatgroup',
-            name='members_online',
+            model_name="chatgroup",
+            name="members_online",
         ),
         migrations.AddField(
-            model_name='chatgroup',
-            name='members_online',
-            field=models.ManyToManyField(blank=True, related_name='online_group', to=settings.AUTH_USER_MODEL),
+            model_name="chatgroup",
+            name="members_online",
+            field=models.ManyToManyField(
+                blank=True, related_name="online_group", to=settings.AUTH_USER_MODEL
+            ),
         ),
     ]
