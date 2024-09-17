e45f7fbe97c04ed386916e6e591c638a64c768dc
chen weichen <newsforcwc@gmail.com>
2024-09-17 13:16:32 +0800

    feat: add editor model

--- editors/models.py
+++ editors/models.py
@@ -1,2 +1,9 @@
 from django.conf import settings
 from django.db import models
+from taggit.managers import TaggableManager
+
+
+class EditorGroup(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    code = models.TextField()
+    language = TaggableManager()
