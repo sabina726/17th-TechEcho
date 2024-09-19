24f364b79c55c45e026eb73d801d12f84406d310
hong <sabina880731@gmail.com>
2024-09-19 12:28:49 +0800

    feat: The sidebar can switch views

--- users/views.py
+++ users/views.py
@@ -4,9 +4,15 @@ from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
 from django.shortcuts import redirect, render, reverse
+from django.template.loader import render_to_string
 from django.utils.http import url_has_allowed_host_and_scheme
 
+from answers.models import Answer
+from blogs.models import Blog
+from questions.models import Question
+from reservations.models import StudentReservation, TeacherSchedule
 from users.forms import UserPhotoForm, UserProfileForm, UsersForm
 
 from .helper import send_forget_password_mail
@@ -50,15 +56,11 @@ def log_in(request):
             messages.success(request, "登入成功")
             return redirect(next_url)
         else:
-            if "username" in form.errors:
-                messages.error(request, "帳號錯誤")
-            if "password1" in form.errors:
-                messages.error(request, "密碼錯誤")
             if "username" and "password1" in form.errors:
                 messages.error(request, "登入失敗，帳號密碼錯誤或尚未註冊")
     else:
         form = AuthenticationForm()
-    return render(request, "users/login.html", {"form": form, "next": next_url})
+    return render(request, "login.html", {"form": form, "next": next_url})
 
 
 def log_out(request):
@@ -80,11 +82,14 @@ def forget_password(request):
         password_reset.forget_password_token = uuid.uuid4()
         password_reset.save()
 
-        send_forget_password_mail(user.email, password_reset.forget_password_token)
+        send_forget_password_mail(user.email, profile.forget_password_token)
         messages.success(request, "重設密碼的郵件已發送。")
+    else:
+        messages.error(request, "找不到此帳號。")
+
         return redirect("users:forget_password")
 
-    return render(request, "users/forget_password.html")
+    return render(request, "forget_password.html")
 
 
 def change_password(request, token):
@@ -114,34 +119,71 @@ def change_password(request, token):
             return redirect("users:change_password", token=token)
 
     context = {"token": token, "user_id": user.id}
-    return render(request, "users/change_password.html", context)
+    return render(request, "change_password.html", context)
 
 
 @login_required
 def profile(request):
-    if request.method == "POST":
-        photo_form = UserPhotoForm(request.POST, request.FILES, instance=request.user)
-        if photo_form.is_valid():
-            photo_form.save()
-            return redirect("users:profile")
-    else:
-        photo_form = UserPhotoForm(instance=request.user)
 
-    context = {"photo_form": photo_form, "user": request.user}
-    return render(request, "users/profile.html", context)
+    drafts = Blog.objects.filter(author=request.user, is_draft=True).order_by(
+        "-created_at"
+    )
+    blogs = Blog.objects.filter(author=request.user, is_draft=False).order_by(
+        "-created_at"
+    )
+
+    questions = Question.objects.filter(user=request.user).order_by("-created_at")[:]
+    answers = (
+        Answer.objects.filter(user=request.user)
+        .select_related("question", "user")
+        .order_by("-created_at")[:]
+    )
+    reservations = StudentReservation.objects.filter(
+        student=request.user
+    ).select_related("schedule__teacher")
+    schedules = TeacherSchedule.objects.filter(teacher=request.user).prefetch_related(
+        "studentreservation_set__student"
+    )
+    context = {
+        "user": request.user,
+        "drafts": drafts,
+        "questions": questions,
+        "answers": answers,
+        "blogs": blogs,
+        "reservations": reservations,
+        "schedules": schedules,
+    }
+    return render(request, "profile.html", context)
 
 
 @login_required
 def profile_edit(request):
     if request.method == "POST":
         form = UserProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
+        photo_form = UserPhotoForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid() and photo_form.is_valid():
             form.save()
-            messages.success(request, "您的個人資料已成功更新。")
-            return redirect("users:profile")
+            photo_form.save()
+            if "HX-Request" in request.headers:
+                response = HttpResponse()
+                response["HX-Redirect"] = reverse("users:profile")
+                return response
+            else:
+                return redirect("users:profile")
         else:
-            messages.error(request, "請更正以下錯誤。")
+            if "HX-Request" in request.headers:
+                html = render_to_string(
+                    "profile_edit.html",
+                    {"form": form, "photo_form": photo_form},
+                    request,
+                )
+                return HttpResponse(html)
+            else:
+                messages.error(request, "請更正以下錯誤。")
     else:
         form = UserProfileForm(instance=request.user)
+        photo_form = UserPhotoForm(instance=request.user)
 
-    return render(request, "users/profile_edit.html", {"form": form})
+    return render(
+        request, "profile_edit.html", {"form": form, "photo_form": photo_form}
+    )
