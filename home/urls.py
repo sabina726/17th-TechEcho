1a273b777c21436c04f98a0e6777ad4aa05a9bff
chi <a27976566@gmail.com>
2024-09-27 11:23:39 +0800

    style: footer

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
+    path("privacy/", views.privacy, name="privacy"),
+    path("terms/", views.terms, name="terms"),
 ]
